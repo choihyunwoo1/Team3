2af5771383b1e7f786612f0c5068d4fbfb421083
DESKTOP-5V3FRMH\M <marhalroutemarhalroute@gmail.com>
2025-12-15 18:30:54 +0900

    세영 - 트리거 스크립트 통합

--- Assets/Seyoung/Scrips/TriggerController.cs
+++ Assets/Seyoung/Scrips/TriggerController.cs
@@ -1,13 +1,28 @@
 using UnityEngine;
 
-public interface TriggerController 
+public class TriggerController : MonoBehaviour
 {
+    public Animator animator;
+    public GameObject triggerObject;
 
-    public void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (!other.CompareTag("Player")) return;
+
+        // 애니메이션 실행
+        if (animator != null)
         {
             animator.SetTrigger("OnTrigger");
         }
+
+        // 오브젝트 활성화
+        if (triggerObject != null)
+        {
+            triggerObject.SetActive(true);
+        }
+
+        // 마지막에 한 번만 제거
+        Destroy(gameObject);
     }
+
 }
