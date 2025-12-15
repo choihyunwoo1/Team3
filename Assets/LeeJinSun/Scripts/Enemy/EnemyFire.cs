68847be18084af4c553f37ff965e6cb8dd198c90
DESKTOP-5V3FRMH\M <jin7466sun@gmail.com>
2025-12-15 18:53:09 +0900

    이진선- 인트로 씬 추가(북페이지 에셋 다운)

---
+++ Assets/LeeJinSun/Scripts/Enemy/EnemyFire.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace JS
+{
+    public class EnemyFire : MonoBehaviour
+    {
+        #region Variables
+
+        #endregion
+
+        #region Unity Event Method
+
+        #endregion
+
+        #region Custom Method
+
+        #endregion
+    }
+}
